5fd597f84066c2dc57161fc34936a44c6bc8db9c
spykins <waleola.akinsanmi@gmail.com>
2018-10-12 19:05:43 -0400

    add environment variable to code

--- db_repository/dbConfig.js
+++ db_repository/dbConfig.js
@@ -3,9 +3,9 @@ const dbName = "contactDb";
 
 const dbConfig = () => {
     return mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password: "Nigeria_1960",
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
         database: dbName
     });
 }
